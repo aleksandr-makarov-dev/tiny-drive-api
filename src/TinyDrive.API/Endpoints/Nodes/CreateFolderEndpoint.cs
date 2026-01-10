132b90f4271fdb66e8608068cef814ef96d07d24
Aleksandr Makarov <alexandr.makarov.2000@gmail.com>
2026-01-10 13:04:05 +0200

    Add CreateFolder and GetFolderItems endpoints

---
+++ src/TinyDrive.API/Endpoints/Nodes/CreateFolderEndpoint.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TinyDrive.API.Extensions;
+using TinyDrive.API.Infrastructure;
+using TinyDrive.Application.Nodes.CreateFolder;
+using TinyDrive.SharedKernel;
+
+namespace TinyDrive.API.Endpoints.Nodes;
+
+internal sealed class CreateFolderEndpoint : IEndpoint
+{
+    private class Request
+    {
+        public string Name { get; init; }
+
+        public Ulid? ParentId { get; init; }
+    }
+
+    public void MapEndpoint(IEndpointRouteBuilder builder)
+    {
+        builder.MapPost("api/nodes/create-folder", async ([FromBody] Request request, ISender sender) =>
+        {
+            var command = new CreateFolderCommand(request.Name, request.ParentId);
+
+            Result<Ulid> result = await sender.Send(command);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        });
+    }
+}
---
+++ src/TinyDrive.API/Endpoints/Nodes/GetFolderItemsEndpoint.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TinyDrive.API.Extensions;
+using TinyDrive.API.Infrastructure;
+using TinyDrive.Application.Nodes.GetFolderItems;
+using TinyDrive.SharedKernel;
+
+namespace TinyDrive.API.Endpoints.Nodes;
+
+public class GetFolderItemsEndpoint : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder builder)
+    {
+        builder.MapGet("api/nodes", async ([FromQuery] Ulid? parentId, ISender sender) =>
+        {
+            var query = new GetFolderItemsQuery(parentId);
+
+            Result<PagedResult<FolderItemResponse>> result = await sender.Send(query);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        });
+    }
+}
