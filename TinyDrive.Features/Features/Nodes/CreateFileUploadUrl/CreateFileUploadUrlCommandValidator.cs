08ec4972f2b6aff62cc05d0461009092ffb22f6f
Aleksandr Makarov <alexandr.makarov.2000@gmail.com>
2026-01-31 18:26:53 +0200

    feat: Add CreateFileUploadUrlCommandValidator

---
+++ TinyDrive.Features/Features/Nodes/CreateFileUploadUrl/CreateFileUploadUrlCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace TinyDrive.Features.Features.Nodes.CreateFileUploadUrl;
+
+public sealed class CreateFileUploadUrlCommandValidator : AbstractValidator<CreateFileUploadUrlCommand>
+{
+	private const long MaxFileSizeBytes = 100 * 1024 * 1024;
+
+	public CreateFileUploadUrlCommandValidator()
+	{
+		RuleFor(x => x.FileName)
+			.NotEmpty()
+			.MaximumLength(255)
+			.Matches(@"^(?!.*\.\.)[A-Za-z0-9._-]+\.[A-Za-z0-9]+$");
+
+		RuleFor(x => x.FileSizeBytes)
+			.GreaterThan(0)
+			.LessThanOrEqualTo(MaxFileSizeBytes);
+
+		RuleFor(x => x.ContentType)
+			.NotEmpty()
+			.MaximumLength(100);
+	}
+}
